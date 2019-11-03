3c58be9ac86c10c9b738f4b23c774b69c48a6d08
erikwesterberg <erik.westerberg@hotmail.se>
2019-11-03 16:33:37 +0100

    creates two different themes thats gonna be toggled between nightmode or not. using themeprovider from styled componenets

--- react-hooks-videoplayer-START/src/components/containers/WbnPlayer.js
+++ react-hooks-videoplayer-START/src/components/containers/WbnPlayer.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
+import { ThemeProvider } from "styled-components";
+import { statement } from "@babel/template";
+
+const theme = {
+  bgcolor: "#353535",
+  bgcolorItewm: "#414141",
+  bgcolorItemActive: "#405c63",
+  bgcolorPlayed: "#526d4e",
+  border: "none",
+  borderPlayed: "none",
+  color: "#fff"
+};
+
+const themeLight = {
+  bgcolor: "#fff",
+  bgcolorItewm: "#fff",
+  bgcolorItemActive: "#80a7b1",
+  bgcolorPlayed: "#7d9979",
+  border: "1px solid #353535",
+  borderPlayed: "none",
+  color: "#353535"
+};
 
 const WbnPlayer = props => {
   return (
-    <>
+    <ThemeProvider theme={statement.nightMode ? theme : themeLight}>
       <Video />
       <Playlist />
-    </>
+    </ThemeProvider>
   );
 };
 
