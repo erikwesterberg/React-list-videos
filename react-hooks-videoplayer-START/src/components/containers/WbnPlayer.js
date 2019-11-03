d52919484dbf8166dbdfa2cb6218f949e9989f36
erikwesterberg <erik.westerberg@hotmail.se>
2019-11-03 19:44:39 +0100

    json.parse the videos and pass the attributes as state inside of a usestate hook

--- react-hooks-videoplayer-START/src/components/containers/WbnPlayer.js
+++ react-hooks-videoplayer-START/src/components/containers/WbnPlayer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
 import { ThemeProvider } from "styled-components";
@@ -26,6 +26,16 @@ const themeLight = {
 };
 
 const WbnPlayer = props => {
+
+  const videos = JSON.parse(document.querySelector('[name="videos"]').value)
+
+  const [state, setState] = useState({
+    videos: videos.playlist,
+    activeVideo: videos.playlist[0],
+    nightMode: true,
+    playlistId: videos.playlistId,
+    autoplay: false,
+  })
 
 const nightModeCallback = () => {
 
