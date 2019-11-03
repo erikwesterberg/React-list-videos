00ebf529b671d7015be9ca62ef44a887f8a6d126
erikwesterberg <erik.westerberg@hotmail.se>
2019-11-03 17:01:31 +0100

    finnishing up with wrapping styling and destructoring props in needed components

--- react-hooks-videoplayer-START/src/components/PlaylistHeader.js
+++ react-hooks-videoplayer-START/src/components/PlaylistHeader.js
@@ -1,5 +1,14 @@
 import React from "react";
+import StyledPLaylistHeader from "./styles/StyledPlaylistHeader";
+import StyledJourney from "./styles/StyledJourney";
 
-const PlaylistHeader = props => <div>PLAYLISTHEADER!!!</div>;
+const PlaylistHeader = ({ active, total}) => (
+  <StyledPLaylistHeader>
+    <p>{active.title}</p>
+    <StyledJourney>
+      {active.num} / {total}
+    </StyledJourney>
+  </StyledPLaylistHeader>
+)
 
 export default PlaylistHeader;
